68204e18d7dd7cb30da06654e38f6272070d349e
Chiziingiin <chiziingiin@126.com>
2022-02-09 23:15:40 +0800

    修复了一些问题

--- Api/index.js
+++ Api/index.js
@@ -7,25 +7,57 @@ function getbody(){
 }
 function getcontent(){
   console.log('[ajax-public.html] ajax start');
-  $.ajax({url:"./public.html",success:function(result){
+  $.ajax({url:"./public.html",success:(result)=>{
     console.log('[ajax-public.html]','success')
     if(result){
-      var title = $("#title").val()
-      var description = $("#description").val();
-      var before = result.split('${content}')[0]
-      var after = result.split('${content}')[1]
-      if(title && title!='')title=title
-      else title="无标题"
-      before=before.replace("${title}",title)
-      if(description && description!='')description=description
-      else {description=getbody();description=description.substring(0,39).replace(RegExp('\r',"g"),' ').replace(RegExp('\n',"g"),' ').replace(RegExp('  ',"g"),'');}
-      before=before.replace("${description}",description)
-      var t = mx.Api.getRandomString(10)
-      console.log(`${before}${tinyMCE.activeEditor.getContent()}${after}\n`)
-      mx.Api.download(`${t}.html`,`${before}${tinyMCE.activeEditor.getContent()}${after}`);
-      imgurl=""
-      mx.alert(`文件相关代码`,`{"title":"${title}","description":"${description}","url":"/p/${t}.html","imgurl":"${imgurl}","date":${(new Date).getTime()}}`,false,function(){},null,"确定")
-      $(".mx-alert-content").select();console.log($(".mx-alert-content").text());document.execCommand("copy");
+      $.ajax({url:"/p/data.json",dataType:'text',success:(e)=>{
+        console.log("[获取data.json]",e,typeof e)
+        var title = $("#title").val()
+        var description = $("#description").val();
+        var imgurl = $("#imgurl").val();
+        if(!imgurl) imgurl='[]'
+        var author = $("#author").val();
+        var authorId = $("#authorId").val();
+        var before = result.split('${content}')[0]
+        var after = result.split('${content}')[1]
+        if(title && title!='')title=title
+        else title="无标题"
+        before=before.replace("${title}",title)
+        if(description && description!='')description=description
+        else {description=getbody();description=description.substring(0,39).replace(RegExp('\r',"g"),' ').replace(RegExp('\n',"g"),' ').replace(RegExp('  ',"g"),'');}
+        before=before.replace("${description}",description)
+        var t = mx.Api.getRandomString(10)
+        console.log('[新生成文章的内容]',[`${before}${tinyMCE.activeEditor.getContent()}${after}\n`])
+        var data = JSON.parse(e);
+        data.push({
+          "title":title,
+          "description":description,
+          "pId":t,
+          "url":`/p/${t}.html`,
+          "author":author,
+          "authorId":authorId,
+          "read":1,
+          "comment":0,
+          "img":JSON.parse(imgurl)
+        })
+        console.log("[载入data]",data)
+
+        mx.Api.download(`${t}.html`,`${before}${tinyMCE.activeEditor.getContent()}${after}`);
+        mx.Api.download(`data.json`,`${JSON.stringify(data)}`);
+        mx.alert("成功")
+        // mx.alert(`文件相关代码`,JSON.stringify({
+        //   "title":title,
+        //   "description":description,
+        //   "pId":t,
+        //   "url":`/p/${t}.html`,
+        //   "author":author,
+        //   "authorId":authorId,
+        //   "read":1,
+        //   "comment":0,
+        //   "img":JSON.parse(imgurl)
+        // }),false,function(){},null,"确定")
+        // $(".mx-alert-content").select();document.execCommand("copy");
+      }})
     } else {
       alert("[ajax][failed - result]")
       alert("[ajax-public.html][failed - result]",result)
